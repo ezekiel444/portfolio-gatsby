e18f6900b28ff92b6280ada2a713f9aec8a1a776
ezekiel444 <ezekielmatomi588@gmail.com>
2021-03-14 20:07:16 +0100

    service updated... 🕊

--- src/constants/services.js
+++ src/constants/services.js
@@ -5,18 +5,18 @@ export default [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Trust me i can give you the best web development experience. Your satisfaction is my comfort`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
     title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `I have design couples of user friendly/intuitive app. I offer a top notch design`,
   },
   {
     id: 3,
     icon: <FaAndroid className="service-icon" />,
     title: "app design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `I deliver your app design as detailed as possible.`,
   },
 ]
