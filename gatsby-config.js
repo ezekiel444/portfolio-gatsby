07a8d93240648788951793f6cb3316f13a544f0d
ezekiel444 <ezekielmatomi588@gmail.com>
2021-03-17 15:44:23 +0100

    config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,15 +29,12 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-strapi`,
+      resolve: "gatsby-source-strapi",
       options: {
         apiURL: process.env.API_URL || "http://localhost:1337",
         queryLimit: 5000, // Default to 100
-        //   contentTypes : `jobs`, `projects`, `blogs`,
-        //   singleType : `about`
-        //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
-        contentTypes: [`jobs`, `projects`,`blogs`],
-        singleTypes: [`about`],
+        contentTypes: ["jobs", "projects","blogs"],
+        singleTypes: ["about"],
       },
     },
     {
