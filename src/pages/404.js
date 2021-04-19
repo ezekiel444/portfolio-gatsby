554a731376b388b2e79e7d670ae81fc89b143519
ezekiel444 <ezekielmatomi588@gmail.com>
2021-04-20 00:37:56 +0200

    404 error fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,17 @@
-// import React from "react"
-// import Layout from "../components/Layout"
-// import { Link } from "gatsby"
-// import Seo from "../components/SEO"
+import React from "react"
+import Layout from "../components/Layout"
+import { Link } from "gatsby"
+import Seo from "../components/SEO"
 
-// const Error = () => {
-//   return <Layout> <main className="error-page">
-//           <Seo title='Error 404' description='Error 404 page'/>
-//     <div className="error-container">
-//       <h1>o0oOps.... Page not found</h1>
-//       <Link to='/' className='btn'>back home</Link>
-//     </div>
-//   </main>
-//   </Layout>
-// }
+const Error = () => {
+  return <Layout> <main className="error-page">
+          <Seo title='Error 404' description='Error 404 page'/>
+    <div className="error-container">
+      <h1>o0oOps.... Page not found</h1>
+      <Link to='/' className='btn'>back home</Link>
+    </div>
+  </main>
+  </Layout>
+}
 
-// export default Error
+export default Error
